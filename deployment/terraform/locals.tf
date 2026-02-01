e972b7ef23ee2255bb0477cbcf510b7d291ad312
tomakady <tomakady@gmail.com>
2026-02-01 17:03:33 +0000

    locals name changed to eks-game

--- deployment/terraform/locals.tf
+++ deployment/terraform/locals.tf
@@ -1,5 +1,6 @@
 locals {
-  name    = "project-vpc"
+  # Used as VPC name and as prefix for subnet names (e.g. project-vpc-public-eu-west-2a).
+  name    = "eks-game"
   domain  = "eks.tomakady.com"
   region  = "eu-west-2"
   cluster = "eks-2048"
